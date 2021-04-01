1cd5a107e78a130ec3038d37e94a2ef0bbc82ba8
lifeisegg123 <leejj2002@naver.com>
2021-04-01 22:21:32 +0900

    fix category bug

--- src/utils/getCategoryOp.ts
+++ src/utils/getCategoryOp.ts
@@ -2,11 +2,11 @@ import { Between, Equal } from 'typeorm';
 
 export const getCategoryOp = (category: number) => {
   if (category % 1000 === 0) {
-    return Between(category, category + 1000);
+    return Between(category, category + 999);
   } else if (category % 100 === 0) {
-    return Between(category, category + 100);
+    return Between(category, category + 99);
   } else if (category % 10 === 0) {
-    return Between(category, category + 10);
+    return Between(category, category + 9);
   } else {
     return Equal(category);
   }
